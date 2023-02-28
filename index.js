f5f150c52746493daaca7acccfaf01377ee70059
Moeid Haider <moeidhaider9@gmail.com>
2023-02-28 11:38:56 +0000

    Project setup

--- index.js
+++
@@ -1,6 +0,0 @@
-const NotesModel = require("./notesModel.js");
-let model = new NotesModel();
-
-console.log(model.addNotes("Buy milk"));
-console.log(model.addNotes("Go to the gym"));
-console.log(model.getNotes());
