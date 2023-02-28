51b9705ea2896937bc08dbb52d055e5d419c761f
Moeid Haider <moeidhaider9@gmail.com>
2023-02-28 11:02:57 +0000

    Used TDD to create the notes app

---
+++ notesModel.js
@@ -0,0 +1,19 @@
+class NotesModel {
+    constructor() {
+      this.notes = [];
+    }
+  
+    addNotes(note) {
+      this.notes.push(note);
+    }
+  
+    reset() {
+      this.notes = [];
+    }
+  
+    getNotes() {
+      return this.notes;
+    }
+}
+  
+module.exports = NotesModel;
